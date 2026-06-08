769b869c652ce6c3a9d2725a7934be1131f76dd3
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-08 14:52:51 -0300

    Adiciona nova classe para ordenar registro por horario

--- src/main/br/furb/banco/utils/RegistroPorHorario.java
+++ src/main/br/furb/banco/utils/RegistroPorHorario.java
@@ -1,4 +1,24 @@
 package br.furb.banco.utils;
 
-public class RegistroPorHorario {
+import br.furb.banco.modelos.RegistroAtendimento;
+
+public class RegistroPorHorario implements Comparable<RegistroPorHorario> {
+    public RegistroAtendimento registro;
+
+    public RegistroPorHorario(RegistroAtendimento registro) {
+        this.registro = registro;
+    }
+
+    public RegistroAtendimento getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(RegistroAtendimento registro) {
+        this.registro = registro;
+    }
+
+    @Override
+    public int compareTo(RegistroPorHorario outro) {
+        return this.registro.getHorarioInicioAtendimento().compareTo(outro.registro.getHorarioInicioAtendimento());
+    }
 }
