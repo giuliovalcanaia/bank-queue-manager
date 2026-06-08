cdd2955b42961a63ffe955edccaf756d139501de
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-08 15:11:55 -0300

    Adiciona javadoc para melhorar documentação

--- src/main/br/furb/banco/utils/RegistroPorHorario.java
+++ src/main/br/furb/banco/utils/RegistroPorHorario.java
@@ -2,21 +2,47 @@ package br.furb.banco.utils;
 
 import br.furb.banco.modelos.RegistroAtendimento;
 
+/**
+ * Classe auxiliar (wrapper) utilizada para ordenação de registros de atendimento
+ * pelo critério cronológico, ou seja, pelo horário de início do atendimento no guichê.
+ * Implementa Comparable para permitir o uso com algoritmos de ordenação
+ * genéricos, como o OrdenacaoQuickSort.
+ */
 public class RegistroPorHorario implements Comparable<RegistroPorHorario> {
+
     public RegistroAtendimento registro;
 
+    /**
+     * Construtor da classe RegistroPorHorario.
+     * @param registro O registro de atendimento a ser encapsulado para ordenação.
+     */
     public RegistroPorHorario(RegistroAtendimento registro) {
         this.registro = registro;
     }
 
+    /**
+     * Retorna o registro de atendimento encapsulado.
+     * @return O objeto RegistroAtendimento associado a este wrapper.
+     */
     public RegistroAtendimento getRegistro() {
         return registro;
     }
 
+    /**
+     * Define o registro de atendimento encapsulado.
+     * @param registro O novo objeto RegistroAtendimento a ser associado.
+     */
     public void setRegistro(RegistroAtendimento registro) {
         this.registro = registro;
     }
 
+    /**
+     * Compara este registro com outro pelo horário de início do atendimento,
+     * em ordem cronológica crescente (atendimento mais antigo primeiro).
+     * @param outro O outro RegistroPorHorario a ser comparado.
+     * @return Valor negativo se este tiver horário anterior, zero se simultâneos,
+     *         ou valor positivo se este tiver horário posterior.
+     */
     @Override
     public int compareTo(RegistroPorHorario outro) {
         return this.registro.getHorarioInicioAtendimento().compareTo(outro.registro.getHorarioInicioAtendimento());
--- src/main/br/furb/banco/utils/RegistroPorTempo.java
+++ src/main/br/furb/banco/utils/RegistroPorTempo.java
@@ -2,21 +2,47 @@ package br.furb.banco.utils;
 
 import br.furb.banco.modelos.RegistroAtendimento;
 
+/**
+ * Classe auxiliar (wrapper) utilizada para ordenação de registros de atendimento
+ * pelo critério de tempo de espera do cliente na fila.
+ * Implementa Comparable para permitir o uso com algoritmos de ordenação
+ * genéricos, como o OrdenacaoQuickSort.
+ */
 public class RegistroPorTempo implements Comparable<RegistroPorTempo> {
+
     private RegistroAtendimento registro;
 
+    /**
+     * Construtor da classe RegistroPorTempo.
+     * @param registro O registro de atendimento a ser encapsulado para ordenação.
+     */
     public RegistroPorTempo(RegistroAtendimento registro) {
         this.registro = registro;
     }
 
+    /**
+     * Retorna o registro de atendimento encapsulado.
+     * @return O objeto RegistroAtendimento associado a este wrapper.
+     */
     public RegistroAtendimento getRegistro() {
         return registro;
     }
 
+    /**
+     * Define o registro de atendimento encapsulado.
+     * @param registro O novo objeto RegistroAtendimento a ser associado.
+     */
     public void setRegistro(RegistroAtendimento registro) {
         this.registro = registro;
     }
 
+    /**
+     * Compara este registro com outro pelo tempo de espera do cliente na fila,
+     * em ordem crescente (menor tempo de espera primeiro).
+     * @param outro O outro RegistroPorTempo a ser comparado.
+     * @return Valor negativo se este tiver menor tempo de espera, zero se iguais,
+     *         ou valor positivo se este tiver maior tempo de espera.
+     */
     @Override
     public int compareTo(RegistroPorTempo outro) {
         return Long.compare(this.registro.getTempoEsperaMinutos(), outro.registro.getTempoEsperaMinutos());
