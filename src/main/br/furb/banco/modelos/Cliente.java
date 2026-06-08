1a0f6aaaeabd8002cf2150b965a95d60df37910a
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-08 14:08:39 -0300

    Adiciona a classe cliente ao projeto

--- src/main/br/furb/banco/modelos/Cliente.java
+++ src/main/br/furb/banco/modelos/Cliente.java
@@ -1,4 +1,67 @@
 package br.furb.banco.modelos;
 
+import java.time.LocalTime;
+
+/**
+ * Classe que representa um Cliente no sistema de atendimento bancário.
+ */
 public class Cliente {
+
+    private int id;
+    private boolean prioritario;
+    private LocalTime horarioChegada;
+
+    /**
+     * Construtor da classe Cliente.
+     * * @param id             Identificador único do cliente.
+     * @param prioritario    Define se o cliente é da fila de prioridade (true) ou normal (false).
+     * @param horarioChegada Horário em que o cliente entrou na fila.
+     */
+    public Cliente(int id, boolean prioritario, LocalTime horarioChegada) {
+        this.id = id;
+        this.prioritario = prioritario;
+        this.horarioChegada = horarioChegada;
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isPrioritario() {
+        return prioritario;
+    }
+
+    public LocalTime getHorarioChegada() {
+        return horarioChegada;
+    }
+
+    // Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrioritario(boolean prioritario) {
+        this.prioritario = prioritario;
+    }
+
+    public void setHorarioChegada(LocalTime horarioChegada) {
+        this.horarioChegada = horarioChegada;
+    }
+
+    @Override
+    public String toString() {
+        String tipo = "Normal";
+        if (prioritario) {
+            tipo = "Prioritário";
+        }
+
+        return "Cliente { " +
+                "ID = " + id +
+                ", Tipo = " + tipo +
+                ", Chegada = " + horarioChegada +
+                " }";
+    }
 }
