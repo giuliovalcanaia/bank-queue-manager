2b8659c861a7a70c9547f8fce78334dd25d848dc
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-09 11:06:54 -0300

    Melhorias nos comentários

--- src/main/br/furb/banco/utils/RegistroPorTempo.java
+++ src/main/br/furb/banco/utils/RegistroPorTempo.java
@@ -2,36 +2,19 @@ package br.furb.banco.utils;
 
 import br.furb.banco.modelos.RegistroAtendimento;
 
-/**
- * Classe auxiliar (wrapper) utilizada para ordenação de registros de atendimento
- * pelo critério de tempo de espera do cliente na fila.
- * Implementa Comparable para permitir o uso com algoritmos de ordenação
- * genéricos, como o OrdenacaoQuickSort.
- */
 public class RegistroPorTempo implements Comparable<RegistroPorTempo> {
 
     private RegistroAtendimento registro;
 
-    /**
-     * Construtor da classe RegistroPorTempo.
-     * @param registro O registro de atendimento a ser encapsulado para ordenação.
-     */
     public RegistroPorTempo(RegistroAtendimento registro) {
         this.registro = registro;
     }
 
-    /**
-     * Retorna o registro de atendimento encapsulado.
-     * @return O objeto RegistroAtendimento associado a este wrapper.
-     */
+    // Getter and setter
     public RegistroAtendimento getRegistro() {
         return registro;
     }
 
-    /**
-     * Define o registro de atendimento encapsulado.
-     * @param registro O novo objeto RegistroAtendimento a ser associado.
-     */
     public void setRegistro(RegistroAtendimento registro) {
         this.registro = registro;
     }
