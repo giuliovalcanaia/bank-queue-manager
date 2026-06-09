1f6396950239547ff4eeffc14b5079187a930e94
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-09 11:03:54 -0300

    Melhorias nos comentários

--- src/main/br/furb/banco/App.java
+++ src/main/br/furb/banco/App.java
@@ -1,8 +1,9 @@
 package br.furb.banco;
 
 import br.furb.banco.modelos.Cliente;
+import br.furb.banco.modelos.TipoAtendimento;
 import br.furb.banco.servicos.GerenciadorAtendimento;
-import br.furb.banco.servicos.RelatorioService;
+import br.furb.banco.servicos.Relatorio;
 
 import java.time.LocalTime;
 
@@ -16,15 +17,15 @@ public class App {
         // ORDEM CRONOLÓGICA DOS EVENTOS
         // Criação e chegada de objetos clientes
         // Simultaneamente, faz a simulação de atendimento pelos guichês
-        gerenciador.adicionarCliente(new Cliente(1, false, LocalTime.of(10, 0)));
-        gerenciador.adicionarCliente(new Cliente(2, true, LocalTime.of(10, 5)));
-        gerenciador.adicionarCliente(new Cliente(3, false, LocalTime.of(10, 10)));
+        gerenciador.adicionarCliente(new Cliente(1, TipoAtendimento.GERAL, LocalTime.of(10, 0)));
+        gerenciador.adicionarCliente(new Cliente(2, TipoAtendimento.PREFERENCIAL, LocalTime.of(10, 5)));
+        gerenciador.adicionarCliente(new Cliente(3, TipoAtendimento.GERAL, LocalTime.of(10, 10)));
         gerenciador.chamarProximo(1, LocalTime.of(10, 10));
-        gerenciador.adicionarCliente(new Cliente(4, true, LocalTime.of(10, 15)));
+        gerenciador.adicionarCliente(new Cliente(4, TipoAtendimento.PREFERENCIAL, LocalTime.of(10, 15)));
         gerenciador.chamarProximo(2, LocalTime.of(10, 16));
-        gerenciador.adicionarCliente(new Cliente(5, false, LocalTime.of(10, 20)));
+        gerenciador.adicionarCliente(new Cliente(5, TipoAtendimento.GERAL, LocalTime.of(10, 20)));
         gerenciador.chamarProximo(3, LocalTime.of(10, 21));
-        gerenciador.adicionarCliente(new Cliente(6, false, LocalTime.of(10, 25)));
+        gerenciador.adicionarCliente(new Cliente(6, TipoAtendimento.PREFERENCIAL, LocalTime.of(10, 25)));
         gerenciador.chamarProximo(2, LocalTime.of(10, 40));
         gerenciador.chamarProximo(1, LocalTime.of(10, 45));
         gerenciador.chamarProximo(3, LocalTime.of(10, 50));
@@ -32,13 +33,13 @@ public class App {
         // Tenta chamar mais um mesmo sem fila
         gerenciador.chamarProximo(1, LocalTime.of(11, 10));
         // Chega mais um cliente e então o guiche 1 chama
-        gerenciador.adicionarCliente(new Cliente(7, false, LocalTime.of(11, 25)));
+        gerenciador.adicionarCliente(new Cliente(7, TipoAtendimento.GERAL, LocalTime.of(11, 25)));
         gerenciador.chamarProximo(1, LocalTime.of(11, 25));
 
 
 
         // Geração do Relatório Final
-        RelatorioService relatorioService = new RelatorioService();
+        Relatorio relatorioService = new Relatorio();
         relatorioService.imprimirRelatorio(gerenciador);
 
         System.out.println("Fim da simulação");
