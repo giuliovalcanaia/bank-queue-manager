3bac7e380ccdb299203958e70721cdaeeca66371
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-10 10:18:08 -0300

    Melhoria na lógica de atribuição de tempoAtendimento

--- src/main/br/furb/banco/modelos/RegistroAtendimento.java
+++ src/main/br/furb/banco/modelos/RegistroAtendimento.java
@@ -10,30 +10,31 @@ public class RegistroAtendimento {
 
     private Cliente cliente;
     private LocalTime horarioInicioAtendimento;
-    private int tempoAtendimento;
+    private LocalTime horarioTerminoAtendimento;
+    private long tempoEspera;
+    private long tempoAtendimento;
+    private TipoAtendimento tipoAtendimento;
 
     /**
      * Construtor da classe RegistroAtendimento.
      * @param cliente Objeto cliente que está sendo atendido (contém ID e horário de entrada).
      * @param horarioInicioAtendimento Horário em que o cliente foi chamado no guichê.
      */
-    public RegistroAtendimento(Cliente cliente, LocalTime horarioInicioAtendimento, int tempoAtendimento) {
+    public RegistroAtendimento(Cliente cliente, TipoAtendimento tipoAtendimento, LocalTime horarioInicioAtendimento) {
         this.cliente = cliente;
+        this.tipoAtendimento = tipoAtendimento;
         this.horarioInicioAtendimento = horarioInicioAtendimento;
-        this.tempoAtendimento = tempoAtendimento;
+        this.tempoEspera = calculaTempoEsperaMinutos();
+        this.tempoAtendimento = calculaTempoAtendimentoMinutos();
     }
 
-    /**
-     * Calcula o tempo de espera do cliente na fila.
-     * Faz a diferença entre o horário de atendimento e o horário de chegada.
-     * @return O tempo de espera em minutos.
-     */
-    public long getTempoEsperaMinutos() {
-        if (cliente == null || cliente.getHorarioChegada() == null || horarioInicioAtendimento == null) {
-            return 0;
-        }
-        // Classe duration é usada para medir o tempo entre dois instantes
+    private long calculaTempoEsperaMinutos() {
         Duration duracao = Duration.between(cliente.getHorarioChegada(), horarioInicioAtendimento);
+        return duracao.toMinutes();
+    }
+
+    private long calculaTempoAtendimentoMinutos() {
+        Duration duracao = Duration.between(horarioInicioAtendimento, horarioTerminoAtendimento);
         return duracao.toMinutes();
     }
 
@@ -47,8 +48,26 @@ public class RegistroAtendimento {
         return horarioInicioAtendimento;
     }
 
-    public int getTempoAtendimento() {
+    public long getTempoAtendimento() {
         return tempoAtendimento;
+    }
+
+    public TipoAtendimento getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    public long getTempoEspera() {
+        return tempoEspera;
+    }
+
+    public LocalTime getHorarioTerminoAtendimento() {
+        return horarioTerminoAtendimento;
+    }
+
+    // Setters
+
+    public void setHorarioTerminoAtendimento(LocalTime horarioTerminoAtendimento) {
+        this.horarioTerminoAtendimento = horarioTerminoAtendimento;
     }
 
     @Override
@@ -57,9 +76,10 @@ public class RegistroAtendimento {
                 " ClienteID = " + cliente.getId() +
                 ", Tipo = " + cliente.getTipoAtendimento().getDescricao() +
                 ", Chegada = " + cliente.getHorarioChegada() +
-                ", Início = " + horarioInicioAtendimento +
-                ", Tempo Atendimento = " + tempoAtendimento + " min" +
-                ", Tempo Espera = " + getTempoEsperaMinutos() + " min" +
+                ", Início = " + getHorarioInicioAtendimento() +
+                ", Término = " + getHorarioTerminoAtendimento()+
+                ", Tempo Espera = " + getTempoEspera() + " min" +
+                ", Tempo Atendimento = " + getTempoAtendimento() + " min" +
                 " }";
     }
 }
