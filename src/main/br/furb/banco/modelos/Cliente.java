b0e26d7f76a9c8957b370bb6cde8d5be178d751f
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-11 15:13:12 -0300

    Remoção javadoc

--- src/main/br/furb/banco/modelos/Cliente.java
+++ src/main/br/furb/banco/modelos/Cliente.java
@@ -11,12 +11,6 @@ public class Cliente {
     private TipoAtendimento tipoAtendimento;
     private LocalTime horarioChegada;
 
-    /**
-     * Construtor da classe Cliente.
-     * * @param id             Identificador único do cliente.
-     * @param tipoAtendimento Define se o cliente é da fila de prioridade.
-     * @param horarioChegada Horário em que o cliente entrou na fila.
-     */
     public Cliente(int id, TipoAtendimento tipoAtendimento, LocalTime horarioChegada) {
         this.id = id;
         this.tipoAtendimento = tipoAtendimento;
